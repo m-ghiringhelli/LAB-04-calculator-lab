b1ad3e0c9056462a89376a49c2467c0b58eacc6f
m-ghiringhelli <mghiring@gmail.com>
2021-10-07 14:01:14 -0700

    working MUTL feature

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { add } from './calculations.js';
 
 import { subtract } from './calculations.js';
 
+import { multiply } from './calculations.js';
+
 // reference needed DOM elements
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
@@ -13,6 +15,11 @@ const subNumber1 = document.getElementById('sub-number-1');
 const subNumber2 = document.getElementById('sub-number-2');
 const subButton = document.getElementById('sub-button');
 const subAnswer = document.getElementById('sub-result');
+
+const mltNumber1 = document.getElementById('mlt-number-1');
+const mltNumber2 = document.getElementById('mlt-number-2');
+const mltButton = document.getElementById('mlt-button');
+const mltAnswer = document.getElementById('mlt-result');
 
 console.log(subNumber1.value);
 console.log(subNumber2.value);
@@ -46,4 +53,17 @@ subButton.addEventListener('click', () => {
     const dif = subtract(num1, num2);
     // update DOM to reflect new value(s)
     subAnswer.textContent = dif;
+});
+
+mltButton.addEventListener('click', () => {
+    //console.log('button clicked');
+
+    // get user input(s)
+    const num1 = +mltNumber1.value;
+    const num2 = +mltNumber2.value;
+
+    // do any needed work with the value(s)
+    const pro = multiply(num1, num2);
+    // update DOM to reflect new value(s)
+    mltAnswer.textContent = pro;
 });
--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,19 @@
 // IMPORT MODULES under test here:
-import { subtract } from '../calculations.js';
+import { multiply } from '../calculations.js';
 
 const test = QUnit.test;
 
 // name your test by what it is testing
-test('subtracts two numbers', (expect) => {
+test('multiply two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
 
-    const expected = 2;
+    const expected = 15;
     
     //Act 
     // Call the function you're testing and set the result to a const
     
-    const actual = subtract(5, 3); // use your function here
+    const actual = multiply(5, 3); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
