80152371e3fb9bd5cff537ecf8411ebcf092a2db
m-ghiringhelli <mghiring@gmail.com>
2021-10-07 13:09:09 -0700

    created addition UI

--- test/tests.js
+++ test/tests.js
@@ -3,17 +3,25 @@
 
 const test = QUnit.test;
 
+function add(x, y) {
+    return x + y;
+} 
+
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('adds two numbera', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 5;
+    const y = 3;
+    const expected = 8;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
