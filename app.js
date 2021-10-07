f4c4cd26825d5dcefbd036efb669424cb2f85b5c
m-ghiringhelli <mghiring@gmail.com>
2021-10-07 13:47:32 -0700

    working ADD feature

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // import functions
 import { add } from './calculations.js';
+
+import { subtract } from './calculations.js';
+
 // reference needed DOM elements
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
@@ -15,8 +18,14 @@ console.log(addAnswer);
 
 // set event listeners 
 addButton.addEventListener('click', () => {
-    console.log('button clicked');
-});
+    //console.log('button clicked');
+
     // get user input(s)
+    const num1 = +addNumber1.value;
+    const num2 = +addNumber2.value;
+
     // do any needed work with the value(s)
+    const sum = add(num1, num2);
     // update DOM to reflect new value(s)
+    addAnswer.textContent = sum;
+});
--- calculations.js
+++ calculations.js
@@ -1,5 +1,5 @@
-export function add(x, y) {
-    return x + y;
+export function add(num1, num2) {
+    return num1 + num2;
 }
 
 export function subtract(x, y) {
