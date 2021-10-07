d934c64166940d7788927495f7c1fd5869d2107a
m-ghiringhelli <mghiring@gmail.com>
2021-10-07 13:38:58 -0700

    TDD for subtract function

--- test/tests.js
+++ test/tests.js
@@ -1,25 +1,21 @@
 // IMPORT MODULES under test here:
-// import { add } from '../calculator.js';
+import { subtract } from '../calculations.js';
 
 const test = QUnit.test;
 
-import { add } from './calculations.js';
-
 // name your test by what it is testing
-test('adds two numbers', (expect) => {
+test('subtracts two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const x = 5;
-    const y = 3;
-    const expected = 8;
+
+    const expected = 2;
     
     //Act 
     // Call the function you're testing and set the result to a const
     
-    const actual = add(x, y); // use your function here
+    const actual = subtract(5, 3); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
-
